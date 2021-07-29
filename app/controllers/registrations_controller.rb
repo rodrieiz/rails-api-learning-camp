7730423008ab4f18612736795f757c77be521e0d
Rodrigo <rodrieiznano@gmail.com>
2021-07-29 20:43:28 -0300

    Sign up

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,8 @@
+class RegistrationsController < DeviseTokenAuth::RegistrationsController
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :gender, :password, :password_confirmation)
+  end
+
+end
