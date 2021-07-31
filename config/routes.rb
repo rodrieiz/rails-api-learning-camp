dfc6b58d7737a7a5d04ed058d16782e311c07ffc
Rodrigo <rodrieiznano@gmail.com>
2021-07-31 17:35:19 -0300

    Modify route for Topics

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :topics
   mount_devise_token_auth_for 'User', at: 'registration', controllers: {registrations: "registrations"}
 
   scope :api, defaults: { format: :json} do
-    
+    resources :topics
   end
 end
