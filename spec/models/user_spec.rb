26ec5da3e9e79010c550b56b3e722f4a12974b0d
Rodrigo <rodrieiznano@gmail.com>
2021-08-03 12:44:34 -0300

    Let blocks in urer test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
   let!(:user1) { create(:user) }
+  let!(:user2) { build(:user, email: user1.email) }
+  let!(:user3) { build(:user, gender: 'GenderTest') }
 
   it 'valid user' do
     expect(user1).to be_valid
   end
 
   it 'invalid user attributes, duplicate email' do
-    @user2 = build(:user, email: user1.email)
-    expect(@user2).to_not be_valid
+    expect(user2).to_not be_valid
   end
 
   it 'invalid user attributes, incorrect gender' do
-    @user3 = build(:user, gender: 'GenderTest')
-    expect(@user3).to_not be_valid
+    expect(user3).to_not be_valid
   end
 end
