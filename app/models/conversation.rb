7df01fd48c5aec94dd1a453e3b832bfec99d8858
Rodrigo <rodrieiznano@gmail.com>
2021-08-26 17:21:17 -0300

    Add validation in Conversation class

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,6 @@
 class Conversation < ApplicationRecord
   belongs_to :user1, class_name: 'User', foreign_key: 'user1'
   belongs_to :user2, class_name: 'User', foreign_key: 'user2'
+
+  validates_uniqueness_of :user1, scope: %i[user2]
 end
