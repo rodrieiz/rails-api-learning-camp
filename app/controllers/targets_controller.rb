8674cb1407583e0909fe39602feb2810d4ad9c2d
Rodrigo <rodrieiznano@gmail.com>
2021-08-27 20:54:51 -0300

    Service for matches

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -2,12 +2,7 @@ class TargetsController < ApplicationController
   def create
     @target = current_user.targets.create(target_params)
     if @target.save
-      @possible_matches = Target.within(@target.radius, origin: @target)
-      @possible_matches = @possible_matches.where(topic: @target.topic).where.not(user: current_user)
-      @possible_matches.each do |item|
-        Conversation.create(user1: User.find(item.user.id), user2: current_user)
-      end
-
+      @matches = FindMatch.new(@target, current_user).search
       @target
     else
       render json: { errors: @target.errors }, status: :unprocessable_entity
