edc341b238ce07b85372c8f3ea60d073687ddbce
Rodrigo <rodrieiznano@gmail.com>
2021-08-27 12:35:41 -0300

    Validate user1 different user2 in conversations

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -11,7 +11,15 @@
 require 'rails_helper'
 
 RSpec.describe Conversation, type: :model do
+  let(:user) { create(:user) }
+  # let(:conversation) { create(:conversation, user1: user, user2: user) }
   subject { build(:conversation) }
   it { should belong_to(:user1) }
   it { should belong_to(:user2) }
+
+  it 'do not create a conversation ' do
+    conversation = Conversation.new(user1: user, user2: user)
+    conversation.validate
+    expect(conversation.errors).to be_present
+  end
 end
