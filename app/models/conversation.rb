0eae3a23debd6db8c02569279d2e4c387332fea7
Rodrigo <rodrieiznano@gmail.com>
2021-08-31 14:08:36 -0300

    Spelling

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -13,9 +13,9 @@ class Conversation < ApplicationRecord
   belongs_to :user2, class_name: 'User', foreign_key: 'user2'
 
   validates :user1, uniqueness: { scope: :user2 }
-  validate :user1_distint_of_user2
+  validate :user1_distinct_of_user2
 
-  def user1_distint_of_user2
+  def user1_distinct_of_user2
     errors.add(:user2, I18n.t('validations.conversation_users')) if user1 == user2
   end
 end
