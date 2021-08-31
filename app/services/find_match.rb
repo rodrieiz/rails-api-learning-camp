248b16142102ed2ad3321ff307283d5d9d5bcf98
Rodrigo <rodrieiznano@gmail.com>
2021-08-31 08:53:22 -0300

    Change name var

--- app/services/find_match.rb
+++ app/services/find_match.rb
@@ -7,8 +7,8 @@ class FindMatch
   def search
     @possible_matches = Target.within(@target.radius, origin: @target)
     @possible_matches = @possible_matches.where(topic: @target.topic).where.not(user: @user)
-    @possible_matches.each do |item|
-      Conversation.create(user1: User.find(item.user.id), user2: @user)
+    @possible_matches.each do |target|
+      Conversation.create(user1: User.find(target.user.id), user2: @user)
     end
     @possible_matches
   end
