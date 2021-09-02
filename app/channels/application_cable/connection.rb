4e38d61c69649875af4b9c4cf0283a7037392872
Rodrigo <rodrieiznano@gmail.com>
2021-09-02 20:53:02 -0300

    Test for connection and conversation channel

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,20 +2,21 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
 
+    def connect
+      self.current_user = find_verified_user(request.params[:token])
+      logger.add_tags 'ActionCable', current_user.id
+    end
+
     private
 
     def find_verified_user(token)
-      session = Session.find_by(token: token)
-      unless session.nil?
-        return session.user
+      # For now the token is the user id
+      user = User.find_by(id: token)
+      unless user.nil?
+        return user
       else
         return reject_unauthorized_connection
       end
-    end
-
-    def connect
-      self.current_user = find_verified_user(request.params(:token))
-      logger.add_tags 'ActionCable', current_user.id
     end
   end
 end
--- spec/channels/conversation_channel_spec.rb
+++ spec/channels/conversation_channel_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe ConversationChannel, type: :channel do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe ApplicationCable::ConversationChannel, type: :channel do
+  let!(:user) { create(:user) }
+  let!(:conversation) { create(:conversation, user1: user) }
+
+  it 'successfully subscribe' do
+    stub_connection current_user: user
+    subscribe
+    expect(subscription).to be_confirmed
+    expect(subscription.current_user).to eq(user)
+  end
+
+  it 'successfully send a message' do
+    stub_connection current_user: user
+    subscribe
+    last_count = Message.count
+    perform :receive, { message: 'Formula 1 is the best topic', conversation_id: conversation.id }
+    expect(Message.count).to eql last_count + 1
+  end
 end
